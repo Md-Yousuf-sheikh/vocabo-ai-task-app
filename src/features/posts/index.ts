1b26fa341ac5af0b7128f9877d9bfb020fa8ca01
Md-Yousuf-sheikh <sheikhyousuf702@gmail.com>
2026-03-23 17:28:11 +0600

    chore: add posts feature barrel exports
    Expose posts feature screen and component modules through a single index entrypoint for cleaner imports.

---
+++ src/features/posts/index.ts
@@ -0,0 +1,5 @@
+export * from "./components/CommentItem";
+export * from "./components/PostCard";
+export * from "./components/SkeletonCard";
+export * from "./screens/PostDetailScreen";
+export * from "./screens/PostsListScreen";
